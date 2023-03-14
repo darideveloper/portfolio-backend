06a9c84b9dd51e54303e758451bad1572852ffcb
darideveloper <darideveloper@gmail.com>
2023-03-13 18:11:16 -0600

    feat (models - admin) command, roadmap, project, projectbadge, projecttool, projectcommand, projectroadmap models

--- api/admin.py
+++ api/admin.py
@@ -20,6 +20,12 @@ class CommandAdmin(admin.ModelAdmin):
     ordering = ('name',)
     search_fields = ('name', 'command')
     
+@admin.register(models.RoadMap)
+class RoadMapAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+    
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'last_login', 'web_page')
@@ -32,3 +38,38 @@ class UserBadgeAdmin(admin.ModelAdmin):
     ordering = ('user', 'badge')
     search_fields = ('user__username', 'badge__name')
     list_filter = ('user__username', 'badge__name')
+    
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'start_date', 'last_update', 'logo', 'desciption')
+    ordering = ('name', 'user', 'start_date', 'last_update')
+    search_fields = ('name', 'user__username', 'desciption', 'details')
+    list_filter = ('user__username', 'start_date', 'last_update')
+    
+@admin.register(models.ProjectBadge)
+class ProjectBadgeAdmin(admin.ModelAdmin):
+    list_display = ('project', 'badge')
+    ordering = ('project', 'badge')
+    search_fields = ('project__name', 'badge__name')
+    list_filter = ('project__name', 'badge__name')
+    
+@admin.register(models.ProjectTool)
+class ProjectToolAdmin(admin.ModelAdmin):
+    list_display = ('project', 'tool')
+    ordering = ('project', 'tool')
+    search_fields = ('project__name', 'tool__name')
+    list_filter = ('project__name', 'tool__name')
+    
+@admin.register(models.ProjectCommand)
+class ProjectCommandAdmin(admin.ModelAdmin):
+    list_display = ('project', 'command', 'tool', 'step', 'command_type')
+    ordering = ('project', 'command', 'tool', 'command_type')
+    search_fields = ('project__name', 'command__name', 'tool__name', 'command__details')
+    list_filter = ('project__name', 'command__name', 'tool__name', 'command_type')
+    
+@admin.register(models.ProjectRoadMap)
+class ProjectRoadMapAdmin(admin.ModelAdmin):
+    list_display = ('project', 'road_map', 'done')
+    ordering = ('project', 'road_map', 'done')
+    search_fields = ('project__name', 'road_map__name')
+    list_filter = ('project__name', 'road_map__name', 'done')
--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
 
 class Badge (models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
@@ -10,9 +10,6 @@ class Badge (models.Model):
     
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name_plural = "Badges"
         
 class Tool (models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
@@ -22,26 +19,102 @@ class Tool (models.Model):
     
     def __str__(self):
         return f"{self.name} ({self.version})"
-
-    class Meta:
-        verbose_name_plural = "Tools"
         
 class Command (models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
     name = models.CharField(max_length=50, unique=True)
-    command = models.TextField()
-    details = models.TextField()
+    command = models.TextField(null=True)
+    details = models.TextField(null=True)
     
     def __str__(self):
         return f"{self.name} ({self.version})"
-
-    class Meta:
-        verbose_name_plural = "Tools"
+        
+class RoadMap (models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class User (AbstractUser):
     web_page = models.URLField (max_length=200, blank=True)
+    
+    def __str__(self):
+        return f"{self.username}"
     
 class UserBadge (models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     badge = models.ForeignKey(Badge, on_delete=models.CASCADE) 
+    
+    def __str__(self):
+        return f"{self.user__username} - {self.badge__name}"
+
+class Project (models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    name = models.CharField(max_length=80, unique=True)
+    start_date = models.DateField(default=timezone.now)
+    last_update = models.DateField(default=timezone.now)
+    logo = models.FileField(upload_to='project_logos', null=True)
+    desciption = models.TextField()
+    details = models.TextField()
+    clone_repo = models.BooleanField(default=False)
+    license = models.CharField(max_length=20, default='MIT')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+class ProjectBadge (models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    badge = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.project__name} - {self.badge__name}"   
+    
+class ProjectTool (models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tool = models.ForeignKey(Tool, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.project__name} - {self.tool__name}"   
+
+class ProjectCommand (models.Model):
+    CHOICES = [
+        ('build', 'Build'), 
+        ('test', 'Test'), 
+        ('deploy', 'Deploy'),
+        ('run', 'Run'),
+        ('install', 'Install'),
+    ]
+    
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    command = models.ForeignKey(Command, on_delete=models.CASCADE)
+    tool = models.ForeignKey(Tool, on_delete=models.CASCADE)
+    step = models.IntegerField(default=1)
+    command_type = models.CharField(
+        max_length = 8,
+        choices = CHOICES,
+        default = 'install'    
+    )
+    
+    def __str__(self):
+        return f"{self.project__name} - {self.command__name}"   
+    
+    # unique "step" for each project
+    def save(self, *args, **kwargs):
+        if self.step is None:
+            self.step = ProjectCommand.objects.filter(project=self.project).count() + 1
+        super(ProjectCommand, self).save(*args, **kwargs)
+
+class ProjectRoadMap (models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    road_map = models.ForeignKey(RoadMap, on_delete=models.CASCADE)
+    done = models.BooleanField(default=True)
+
+
+    
