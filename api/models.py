a2a7650dcf73f9c05e8a021863eecb391c3b8363
darideveloper <darideveloper@gmail.com>
2023-03-24 09:28:56 -0600

    style (admin) hover links color

--- api/models.py
+++ api/models.py
@@ -31,7 +31,7 @@ class Project (models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, serialize=True)
     name = models.CharField(max_length=80, unique=True)
     start_date = models.DateField(default=timezone.now)
-    last_update = models.DateField(default=timezone.now)
+    last_update = models.DateField(auto_now_add=True)
     logo = models.URLField(max_length=200, null=True)
     description = models.TextField()
     details = models.TextField()
@@ -42,6 +42,7 @@ class Project (models.Model):
     tools = models.ManyToManyField(Tool)
     commands = models.TextField(null=True)
     roadmap = models.TextField(null=True)
+    web_page = models.URLField(max_length=200, null=True)
     
     def __str__(self):
         return f"{self.name}"
